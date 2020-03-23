6eb426dfaa3bb0a3e498b5e07dc6f2c6ba160f12
ramakrishnanandas <simple.natural.99@gmail.com>
2020-03-24 01:15:59 +0800

    modefied lables

--- info.php
+++ info.php
@@ -1,6 +1,5 @@
 <?php
-echo '<strong>HELLO WORLD today is </strong><br/>';
-echo date("l, F jS, Y");
-echo '<strong>AUM SRI SAIRAM </strong><br/>';
+echo '<strong>HELLO WORLD </strong><br/>';
+
 phpinfo();
 ?>
